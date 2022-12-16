0cf7f804e937873db56e79c66c3d9ca456109043
Bismark Arroyo <bis.arroyo@gmail.com>
2022-12-15 22:52:15 -0600

    fixed sizes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+// import { ChakraProvider } from '@chakra-ui/react'
 
 import './styles/style.scss'
 // Swiper styles
@@ -11,8 +11,8 @@ import App from './routes/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+
+    <App />
+
   </React.StrictMode>
 )
