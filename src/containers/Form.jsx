81bee2fbc7f389131516033b970e646c46380311
Bismark Arroyo <bis.arroyo@gmail.com>
2023-01-09 23:01:35 -0600

    Added react hook form and state

--- src/containers/Form.jsx
+++ src/containers/Form.jsx
@@ -1,11 +1,110 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { useInView } from 'framer-motion'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import emailjs from '@emailjs/browser'
+import { useForm } from 'react-hook-form'
+
+// import containers
+import Loader from '../components/Loader'
+
+const schema = yup.object().shape({
+  fullName: yup.string().required('Por favor ingrese su nombre').min('Debe tener al menos 2 caracteres'),
+  email: yup.string().email('Debe ingresar un correo electrónico válido').required('Debe tener al menos 2 caracteres'),
+  message: yup.string().required('Por favor escribe un mensaje')
+})
 
 const Form = () => {
+  const form = useRef()
+
+  const { register, handleSubmit, formState: { errors }, reset } = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  const [contactData, setContactData] = useState({
+    error: null,
+    loading: false
+  })
+
+  const { loading, error } = contactData
+
+  const sendEmail = (e) => {
+    setContactData({
+      ...contactData,
+      loading: true
+    })
+    emailjs.sendForm('service_5qlkffn', 'template_4do2mlk', form.current, 'user_6jm6KpxJVyCrP322EtqhO')
+      .then((result) => {
+        setContactData({
+          button: 'Message Sent!',
+          loading: false
+        })
+        reset({
+          fullName: '',
+          email: '',
+          message: ''
+        })
+      }, (err) => {
+        setContactData({
+          ...contactData,
+          error: 'Message not sent, please try again'
+        })
+        console.log(err)
+      })
+  }
+
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
+
   return (
-    <div>
-      <form>
+    <div
+      className=''
+      ref={ref}
+      style={{
+        transform: isInView ? 'none' : 'translateY(50px)',
+        opacity: isInView ? 1 : 0,
+        transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s'
+      }}
+    >
+      <form ref={form} onSubmit={handleSubmit(sendEmail)}>
         <div>
-          <p>form</p>
+          <label>
+            Nombre
+            <input
+              type='text'
+              placeholder='Nombre'
+              {...register('fullName', { required: true })}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Correo electrónico
+            <input
+              type='email'
+              placeholder='Correo Electrónico'
+              {...register('email', { required: true })}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Mensaje
+            <textarea
+              rows='3'
+              type='text'
+              placeholder='Mensaje'
+              {...register('message', { required: true })}
+            />
+          </label>
+        </div>
+
+        <div>
+          <button>
+            {loading ? <Loader /> : 'Enviar'}
+          </button>
         </div>
       </form>
     </div>
--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -1,20 +1,64 @@
-import React from 'react'
+import { useInView } from 'framer-motion'
+import { useRef } from 'react'
 import homeResponsive from '../assets/home-responsive.png'
 
 const Hero = () => {
+  const refTitle = useRef(null)
+  const refText = useRef(null)
+  const refImage = useRef(null)
+  const refBtn = useRef(null)
+  const isInViewTitle = useInView(refTitle, { once: true })
+  const isInViewText = useInView(refText, { once: true })
+  const isInViewImage = useInView(refImage, { once: true })
+  const isInViewBtn = useInView(refBtn, { once: true })
+
   return (
     <div className='hero'>
-      <h1>Somos Nous</h1>
+      <h1
+        ref={refTitle}
+        style={{
+          transform: isInViewTitle ? 'none' : 'translateY(-25px)',
+          opacity: isInViewTitle ? 1 : 0,
+          transition: 'all 0.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s'
+        }}
+      >
+        Somos Nous
+      </h1>
       <div className='flex-fluid'>
-        <div className='hero-text w-40'>
+        <div
+          className='hero-text w-40'
+          ref={refText}
+          style={{
+            transform: isInViewText ? 'none' : 'translateY(50px)',
+            opacity: isInViewText ? 1 : 0,
+            transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s'
+          }}
+        >
           <h2 className='primary'>Diseño web a tu medida</h2>
           <p className='primary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero cum modi soluta sequi. Perferendis iure laboriosam nostrum, labore sit fuga in nesciunt ullam, saepe, optio obcaecati a beatae rem alias.</p>
           <div className='hero-button'>
             <a href='' className='btn-primary'>Servicios</a>
-            <a href='' className='btn-secondary'>Comenzar</a>
+            <a
+              href=''
+              className='btn-secondary'
+              ref={refBtn}
+              style={{
+                // width: isInViewBtn ? '100px' : '0',
+                // transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s'
+              }}
+            >Comenzar
+            </a>
           </div>
         </div>
-        <div className='hero-image w-40'>
+        <div
+          className='hero-image w-40'
+          ref={refImage}
+          style={{
+            transform: isInViewImage ? 'none' : 'translateY(50px)',
+            opacity: isInViewImage ? 1 : 0,
+            transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s'
+          }}
+        >
           <img src={homeResponsive} alt='Home responsive image' />
         </div>
       </div>
